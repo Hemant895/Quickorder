f72229238bd3941612cbe76c9f56c42e955e8e33
Hemant895 <hemantkhedkar102034@gmail.com>
2024-04-08 18:37:07 +0530

    integrated placed order api

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -12,7 +12,20 @@ export class JwtInterceptor implements HttpInterceptor {
 
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  // intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  //   return next.handle(request);
+  // }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const currentUser = localStorage.getItem('token');
+    
+    if (currentUser) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser}`
+        }
+      });
+    }
+
     return next.handle(request);
   }
 }
